d62d176331b3618baa03515329fa827ae8d2b6c3
Venu Madhavi <94669721+Madhavipothina@users.noreply.github.com>
2021-12-16 13:51:45 +0000

    coding

---
+++ p2original.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int input();
+int add(int a, int b);
+void output (int  a ,int b,int sum);
+int main()
+{
+ int a,b,sum;
+ printf("enter two sum numbers\n");
+ a=input();
+ b=input();
+ sum=add(a,b);
+ output(a,b,sum); 
+} 
+int input()
+{
+  int a;
+  scanf("%d", &a);
+  return a;
+}
+ int add(int a, int b)
+{
+  int sum=a+b;
+  return sum;
+}
+void output(int a,int b,int sum)
+{
+  printf("sum of %d and %d = %d\n", a ,b ,sum);
+}

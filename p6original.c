aa06be2d769eb03d733c0cb49af689d853ff48ef
Venu Madhavi <94669721+Madhavipothina@users.noreply.github.com>
2022-01-10 06:03:43 +0000

    p0 p

---
+++ p6original.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int input_n();
+int sum_n(int n);
+void output(int n,int sum);
+int main()
+{
+  int i,sum , 
+}
+int input_n()
+{
+ int i;
+  for(i=0,i<n,i++);
+  a[i]=i;
+}
+int sum n(int n) 
+{
+ sum=sum+a[i] ;
+ return sum;
+}
+void output(int n,int sum)
+{
+  printf("sum of %d numbers is %d\n", i, sum );
+}
